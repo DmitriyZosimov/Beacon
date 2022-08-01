8853c14024af15676d830cbdd00dfe14b6ce1c8e
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-08-01 19:18:03 +0300

    added url to keycloak

--- frontend/market/src/environments/environment.ts
+++ frontend/market/src/environments/environment.ts
@@ -5,7 +5,14 @@
 export const environment = {
   production: false,
   catalogServer: 'http://localhost:8010',
-  searchServer: 'http://localhost:8020'
+  searchServer: 'http://localhost:8020',
+
+  //Authentication
+  clientId: 'beacon-market',
+  redirectUri:"http://localhost:4200/mobile",
+  tokenUrl: 'http://localhost:8180/realms/catalog/protocol/openid-connect/token',
+  authUrl: 'http://localhost:8180/realms/catalog/protocol/openid-connect/auth',
+  logoutUrl: 'http://localhost:8180/realms/catalog/protocol/openid-connect/logout'
 };
 
 /*
