3e42a6e44e131b5923237a3e14b78feeb18e550e
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-08-01 19:25:53 +0300

    added actions for sign in and logout buttons

--- frontend/market/src/app/components/login/login.component.ts
+++ frontend/market/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../service/auth/authentication.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public isLoggedIn = false;
+
+  constructor(private service: AuthenticationService) {
+  }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.service.checkCredentials();
+    let i = window.location.href.indexOf('code');
+    if (!this.isLoggedIn && i != -1) {
+      this.service.retrieveToken(window.location.href.substring(i + 5));
+    }
+  }
+
+  login() {
+    this.service.login();
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.service.logout();
   }
 
 }
