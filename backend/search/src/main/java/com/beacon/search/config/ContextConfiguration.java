1996329d7104f7bc9a6d82d4668d78795837ce38
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-08-17 18:21:36 +0300

    changed configuration of search on account of tie of mobile and shop

--- backend/search/src/main/java/com/beacon/search/config/ContextConfiguration.java
+++ backend/search/src/main/java/com/beacon/search/config/ContextConfiguration.java
@@ -1,5 +1,13 @@
 package com.beacon.search.config;
 
+import com.beacon.model.selialization.DefaultMapDeserializer;
+import com.beacon.model.selialization.DefaultMapSerializer;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+import com.fasterxml.jackson.databind.type.MapType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,6 +20,25 @@ public class ContextConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+
+    @Bean(name = "customSimpleModule")
+    public SimpleModule module() {
+        SimpleModule module = new SimpleModule();
+        module.setSerializerModifier(new BeanSerializerModifier() {
+            @Override
+            public JsonSerializer<?> modifyMapSerializer(SerializationConfig config, MapType valueType, BeanDescription beanDesc, JsonSerializer<?> serializer) {
+                return new DefaultMapSerializer();
+            }
+        });
+
+        module.setDeserializerModifier(new BeanDeserializerModifier() {
+            @Override
+            public JsonDeserializer<?> modifyMapDeserializer(DeserializationConfig config, MapType type, BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
+                return new DefaultMapDeserializer(type, config.getTypeFactory());
+            }
+        });
+        return module;
     }
 
 }
