448a2400803a1242056d7108f2bb9431870fa2f4
DmitriyZosimov <Dmitriy_Zosimov@epam.com>
2022-09-05 21:26:07 +0300

    added @ngrx/store-devtools

--- frontend/market/src/app/core/@ngrx/root-store.module.ts
+++ frontend/market/src/app/core/@ngrx/root-store.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// @ngrx
 import {StoreModule} from "@ngrx/store";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { CartStoreModule } from '../../modules/cart/@ngrx/cart-store.module';
 import {metaReducers} from "./meta-reducers";
-
-
+import { environment } from '../../../environments/environment';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    StoreModule.forRoot({}, {metaReducers}),
     CartStoreModule,
+    StoreModule.forRoot({}, {metaReducers}),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ]
 })
 export class RootStoreModule { }
