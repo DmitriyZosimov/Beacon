dfe735ec242f6b42d9881c5f818617cb797a3fd2
Dmitriy Zosimov <dmitriy_zosimov@epam.com>
2023-02-13 21:09:45 +0300

    fix name

--- .github/workflows/buildVersion.sh
+++ .github/workflows/buildVersion.sh
@@ -16,17 +16,17 @@ if [ "$LAST_BUILD_VERSION" = "" ]; then
 #    exit 1
 # }
  echo "Finished creating initial tag version: $NEXT_BUILD_VERSION"
- echo "$NEXT_BUILD_VERSION" >.github/workflows/buildversion.txt
+ echo "$NEXT_BUILD_VERSION" >.github/workflows/buildVersion.txt
  exit 0
 fi
 echo "The latest build version is $LAST_BUILD_VERSION"
 SECOND_TAG_NUMBER=$(echo "$LAST_BUILD_VERSION" | cut -d. -f2)
-SECOND_FILE_NUMBER=$(cat .github/workflows/buildversion.txt | cut -d. -f2)
+SECOND_FILE_NUMBER=$(cat .github/workflows/buildVersion.txt | cut -d. -f2)
 if [ "SECOND_TAG_NUMBER" = "SECOND_FILE_NUMBER" ]; then
   NEXT_BUILD_VERSION=$(cat .github/workflows/buildversion.txt | cut -d. -f1-2).0
 else
   BUILD_NUMBER=$(echo "$LAST_BUILD_VERSION" | cut -d. -f3)
-  NEXT_BUILD_VERSION=$(cat .github/workflows/buildversion.txt | cut -d. -f1-2).$((BUILD_NUMBER + 1))
+  NEXT_BUILD_VERSION=$(cat .github/workflows/buildVersion.txt | cut -d. -f1-2).$((BUILD_NUMBER + 1))
 fi
 
 echo "The next build version is $NEXT_BUILD_VERSION"
@@ -41,7 +41,7 @@ git tag -d "$NEXT_BUILD_VERSION" || true
 #    exit 1
 # }
 
-echo "$NEXT_BUILD_VERSION" >.github/workflows/buildversion.txt
+echo "$NEXT_BUILD_VERSION" >.github/workflows/buildVersion.txt
 
 exit 0
 
