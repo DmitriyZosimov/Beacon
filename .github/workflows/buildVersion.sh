1e9c7f23ad23d3b457cd73ac44bd2f5f34bc8999
DmitriyZosimov <dmitriy_zosimov@epam.com>
2023-02-14 18:30:37 +0300

    fix

--- .github/workflows/buildVersion.sh
+++ .github/workflows/buildVersion.sh
@@ -2,8 +2,8 @@
 LAST_TAG=$(git describe --match "*" --abbrev=0 --tags "$(git rev-list --tags --max-count=1)")
 echo "The latest tag version: $LAST_TAG"
 LAST_BUILD_VERSION=$LAST_TAG
-git config --global user.email "beacon@beacon.com"
-git config --global user.name "GitHub Action"
+git config --global user.email "dmitriy_zosimov@epam.com"
+git config --global user.name "DmitriyZosimov"
 if [ "$LAST_BUILD_VERSION" = "" ]; then
   NEXT_BUILD_VERSION="0.0.1"
   echo "There are currently no tag version. Tagging commit with version $NEXT_BUILD_VERSION"
@@ -24,7 +24,8 @@ fi
 echo "The latest build version is $LAST_BUILD_VERSION"
 SECOND_TAG_NUMBER=$(echo "$LAST_BUILD_VERSION" | cut -d. -f2)
 SECOND_FILE_NUMBER=$(cat .github/workflows/buildVersion.txt | cut -d. -f2)
-if [ "SECOND_TAG_NUMBER" == "SECOND_FILE_NUMBER" ]; then
+if [ "$SECOND_TAG_NUMBER" = "$SECOND_FILE_NUMBER" ];
+then
   BUILD_NUMBER=$(echo "$LAST_BUILD_VERSION" | cut -d. -f3)
   NEXT_BUILD_VERSION=$(cat .github/workflows/buildVersion.txt | cut -d. -f1-2).$((BUILD_NUMBER + 1))
 else
