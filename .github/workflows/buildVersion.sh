f72733dd2cea03b53277dfc438cd7de2ce687b2c
DmitriyZosimov <dmitriy_zosimov@epam.com>
2023-02-20 19:56:05 +0300

    test

--- .github/workflows/buildVersion.sh
+++ .github/workflows/buildVersion.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+if [ "$TOKEN" = "" ]; then
+  echo "The token is not available, can not set build version"
+  exit 1
+fi
+
+GITHUB_URL = "https://$TOKEN@github.com/DmitriyZosimov/Beacon.git"
 LAST_TAG=$(git describe --match "*" --abbrev=0 --tags "$(git rev-list --tags --max-count=1)")
 echo "The latest tag version: $LAST_TAG"
 LAST_BUILD_VERSION=$LAST_TAG
@@ -8,12 +15,12 @@ if [ "$LAST_BUILD_VERSION" = "" ]; then
   NEXT_BUILD_VERSION="0.0.1"
   echo "There are currently no tag version. Tagging commit with version $NEXT_BUILD_VERSION"
   git tag -d "$NEXT_BUILD_VERSION" || true
-  git push --delete origin "$NEXT_BUILD_VERSION" || true
+  git push --delete "$GITHUB_URL" "$NEXT_BUILD_VERSION" || true
   git tag "$NEXT_BUILD_VERSION" || {
     echo "ERROR: Tagging commit failed"
     exit 1
   }
-  git push origin --tags || {
+  git push "$GITHUB_URL" --tags || {
     echo "ERROR: Pushing tags failed"
     exit 1
  }
@@ -34,7 +41,7 @@ fi
 
 echo "The next build version is $NEXT_BUILD_VERSION"
 git tag -d "$NEXT_BUILD_VERSION" || true
-  git push --delete origin "$NEXT_BUILD_VERSION" || true
+  git push --delete "$GITHUB_URL" "$NEXT_BUILD_VERSION" || true
   echo "$NEXT_BUILD_VERSION" >.github/workflows/buildVersion.txt
   git add .github/workflows/buildVersion.txt
   git commit -m 'version $NEXT_BUILD_VERSION'
@@ -42,7 +49,7 @@ git tag -d "$NEXT_BUILD_VERSION" || true
     echo "ERROR: Tagging commit failed"
     exit 1
   }
-  git push -u origin master || {
+  git push -f "$GITHUB_URL" || {
     echo "ERROR: Pushing tags failed"
     exit 1
  }
