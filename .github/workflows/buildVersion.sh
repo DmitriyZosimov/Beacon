e36e56af7cd267f03ce2363c08528fb72cfa83c0
DmitriyZosimov <dmitriy_zosimov@epam.com>
2023-02-23 19:05:29 +0300

    updated user name and email

--- .github/workflows/buildVersion.sh
+++ .github/workflows/buildVersion.sh
@@ -4,9 +4,8 @@ if [ "$TOKEN" = "" ]; then
   echo "The token is not available, can not set build version"
   exit 1
 fi
-echo "GIT USER NAME $GIT_USER_NAME"
-git config --local user.name $GIT_USER_NAME
-git config --local user.email $GIT_USER_EMAIL
+git config --local user.name DmitriyZosimov
+git config --local user.email Dmitriy_Zosimov@epam.com
 
 GITHUB_URL="https://$TOKEN@github.com/DmitriyZosimov/Beacon.git"
 git remote set-url origin "$GITHUB_URL"
