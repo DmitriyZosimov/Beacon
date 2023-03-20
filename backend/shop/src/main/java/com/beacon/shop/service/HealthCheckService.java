3e0623566fd162fbaf80be7bd46b9001a4fd1bba
DmitriyZosimov <dmitriy_zosimov@epam.com>
2023-03-20 12:31:06 +0300

    added statuscheck and resourcecheck endpoints

---
+++ backend/shop/src/main/java/com/beacon/shop/service/HealthCheckService.java
@@ -0,0 +1,12 @@
+package com.beacon.shop.service;
+
+import com.beacon.model.status.StatusBean;
+
+public interface HealthCheckService {
+
+    /**
+     * Resource checks
+     * @return {@link StatusBean#OK} or {@link StatusBean#FAILED}
+     */
+    public StatusBean getResourceStatus();
+}
