777523cab1265fa75f24e7b7006166e3d5c998f0
DmitriyZosimov <dmitriy_zosimov@epam.com>
2023-03-20 12:30:15 +0300

    added statuscheck and resourcecheck endpoints

---
+++ backend/catalog/src/main/java/com/beacon/catalog/web/HealthCheckController.java
@@ -0,0 +1,24 @@
+package com.beacon.catalog.web;
+
+import com.beacon.catalog.service.HealthCheckService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    private final HealthCheckService healthCheckService;
+
+    @GetMapping("/statuscheck")
+    public ResponseEntity<String> getStatusCheck() {
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/resourcecheck")
+    public ResponseEntity<String> getResourceCheck() {
+        return ResponseEntity.ok(healthCheckService.getResourceStatus().name());
+    }
+}
---
+++ backend/catalog/src/test/java/com/beacon/catalog/web/HealthCheckControllerIT.java
@@ -0,0 +1,76 @@
+package com.beacon.catalog.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class HealthCheckControllerIT {
+
+    private static final String STATUS_URL = "/statuscheck";
+    private static final String RESOURCE_URL = "/resourcecheck";
+
+    @Autowired
+    private HealthCheckController healthCheckController;
+    @MockBean
+    private EntityManager entityManager;
+    @MockBean
+    private JwtDecoder jwtDecoder;
+    @MockBean
+    private Query query;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(healthCheckController).build();
+    }
+
+    @Test
+    public void testStatusCheck() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(STATUS_URL))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("OK"));
+    }
+
+    @Test
+    public void testResourceCheck_SUCCESS() throws Exception {
+        when(entityManager.createNativeQuery(anyString())).thenReturn(query);
+        when(query.getSingleResult()).thenReturn("1");
+        mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE_URL))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("OK"));
+    }
+
+    @Test
+    public void testResourceCheck_FAILED() throws Exception {
+        when(entityManager.createNativeQuery(anyString())).thenReturn(query);
+        when(query.getSingleResult()).thenReturn("2");
+        mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE_URL))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("FAILED"));
+    }
+
+    @Test
+    public void testResourceCheck_FAILED_WhenExceptionWasThrown() throws Exception {
+        when(entityManager.createNativeQuery(anyString())).thenReturn(query);
+        when(query.getSingleResult()).thenThrow(NoResultException.class);
+        mockMvc.perform(MockMvcRequestBuilders.get(RESOURCE_URL))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("FAILED"));
+    }
+}
